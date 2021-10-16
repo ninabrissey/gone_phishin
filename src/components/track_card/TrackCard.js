546340d622f2df8624a152a5001a1331b104ce55
Nina Brissey <nbrissey@gmail.com>
2021-10-15 18:41:08 -0600

    Change isPartyMode to dark

--- src/components/track_card/TrackCard.js
+++ src/components/track_card/TrackCard.js
@@ -6,19 +6,21 @@ import './TrackCard.css';
 
 const TrackCard = ({ track }) => {
   const { getCurrentTrack } = useContext(ShowContext);
-  const { isPartyMode, partyMode, basic } = useTheme();
-  const theme = isPartyMode ? partyMode : basic;
+  const { dark, partyMode, basic } = useTheme();
+  const theme = dark ? partyMode : basic;
   const { id, title } = track;
 
   return (
     <article
       className="card"
       id={id}
-      style={{
-        background: theme.primaryBG,
-        boxShadow: theme.shadow,
-        borderRadius: theme.radius,
-      }}
+      style={
+        {
+          // background: theme.primaryBG,
+          // boxShadow: theme.shadow,
+          // borderRadius: theme.radius,
+        }
+      }
       onClick={() => getCurrentTrack(id)}
     >
       <p className="song-title" style={{ color: theme.primaryText }}>
