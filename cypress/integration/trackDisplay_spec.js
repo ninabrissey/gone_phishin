0a527809f79b767a7d59f11f52732ab5b77e3433
Justin Anthony <justincanthony@gmail.com>
2021-10-15 18:23:52 -0600

    Fix date format

--- cypress/integration/trackDisplay_spec.js
+++ cypress/integration/trackDisplay_spec.js
@@ -27,7 +27,7 @@ describe('Track Display', () => {
       .url()
       .should('include', 'shows/4')
       .get('.show-info')
-      .contains('2019/02/21');
+      .contains('Feb 21, 2019');
     cy.contains('TEST NAME 4');
     cy.contains('TEST LOCATION 4');
     cy.get('.song-title').eq(0).contains('Glide').click();
