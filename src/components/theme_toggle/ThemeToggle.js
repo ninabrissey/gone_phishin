61ecfb14b75fec350e06d0c7234bb0fb4279f295
Nina Brissey <nbrissey@gmail.com>
2021-10-15 18:39:56 -0600

    Change isPartyMode to dark

--- src/components/theme_toggle/ThemeToggle.js
+++ src/components/theme_toggle/ThemeToggle.js
@@ -5,13 +5,13 @@ import standardModeIcon from '../../standard-mode-icon.png';
 import './ThemeToggle.css';
 
 const ThemeToggle = () => {
-  const { isPartyMode, toggleTheme } = useTheme();
+  const { dark, toggleTheme } = useTheme();
   // return <button onClick={toggleTheme}>Change Theme</button>;
   return (
     // <div></div>
     <img
       className="mode-icon"
-      src={isPartyMode ? partyModeIcon : standardModeIcon}
+      src={dark ? partyModeIcon : standardModeIcon}
       onClick={toggleTheme}
     />
   );
