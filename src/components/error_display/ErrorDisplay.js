374999ad7522dfc4e0ff7d0059a3212e1c265e42
Nina Brissey <nbrissey@gmail.com>
2021-10-15 18:34:29 -0600

    Change isPartyMode to dark

--- src/components/error_display/ErrorDisplay.js
+++ src/components/error_display/ErrorDisplay.js
@@ -5,15 +5,20 @@ import underConstructionParty from '../../error_party.png';
 import { useTheme } from '../../contexts/ThemeContext';
 
 const ErrorDisplay = ({ message }) => {
-  const { isPartyMode } = useTheme();
-  const errorImage = isPartyMode ? underConstructionParty : underConstruction;
+  const { dark } = useTheme();
+  const errorImage = dark ? underConstructionParty : underConstruction;
 
   return (
-    <div className='error-display'>
+    <div className="error-display">
       <h2>Uh oh. {message}</h2>
       <h3>The show has been delayed.</h3>
-      <img src={errorImage} alt='under construction' className="error-background" ></img>
-    </div>);
+      <img
+        src={errorImage}
+        alt="under construction"
+        className="error-background"
+      ></img>
+    </div>
+  );
 };
 
 export default ErrorDisplay;
